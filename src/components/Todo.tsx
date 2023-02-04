8a7f736b218d85e41b16ce143b591320e325519e
Micael Jerry <hei.jerry.2@gmail.com>
2023-02-04 15:56:40 +0300

    remove initial state render todo

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -4,18 +4,7 @@ import InputTodo from "./InputTodo/InputTodo";
 import RenderTodo from "./RenderTodo/RenderTodo";
 
 const Todo: React.FC<{}> = () => {
-    const [todoList, setTodoList] = useState<TodoType[]>([
-        {
-            title: "title_1",
-            description: "description_1",
-            completed: false,
-        },
-        {
-            title: "title_2",
-            description: "description_2",
-            completed: true,
-        },
-    ]);
+    const [todoList, setTodoList] = useState<TodoType[]>([]);
 
     function checkList(event: any) {
         setTodoList((state) => {
